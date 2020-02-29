9bbb66e28be5581724abd8332fb73208edd034a7
ThomasMoorePizon <60993664+ThomasMoorePizon@users.noreply.github.com>
2020-02-28 19:45:15 -0500

    Add files via upload

---
+++ timedeltaobject.py
@@ -0,0 +1,8 @@
+"""This program creates a timedelta object for 100 days, 10 hours, and 13 minutes"""
+from datetime import timedelta
+data = timedelta(days=100, hours=10, minutes=13)
+print("A timedelta object for 100 days, 10 hours, and 13 minutes")
+print (data)
+
+
+
